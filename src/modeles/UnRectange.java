29ecda8d7dab8fdab33f7e9788beb59a06a00d0e
Louis Friedrich <louisf.28@hotmail.fr>
2020-05-04 10:44:11 +0200

    intialise modèles

---
+++ src/modeles/UnRectange.java
@@ -0,0 +1,10 @@
+package modeles;
+
+/**
+ * 
+ * @author Clément DOSDA, Louis FRIEDRICH, Loïc STEINMETZ, Julien TAVERNIER
+ *
+ */
+public class UnRectange extends UnPolygone {
+
+}
---
+++ src/modeles/UnTriangle.java
@@ -0,0 +1,10 @@
+package modeles;
+
+/**
+ * 
+ * @author Clément DOSDA, Louis FRIEDRICH, Loïc STEINMETZ, Julien TAVERNIER
+ *
+ */
+public class UnTriangle extends UnPolygone {
+
+}
