bc8c26003177c5a9194fa0cacb44b2bcef0c8193
Julien Tavernier <ju.tavernier@hotmail.com>
2020-05-05 19:49:38 +0200

    ajout des boutons

--- src/vues/MenuVue.java
+++ src/vues/MenuVue.java
@@ -5,8 +5,12 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.util.Observable;
 import java.util.Observer;
-
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -24,34 +28,72 @@ public class MenuVue extends JPanel implements Observer {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public static final int LARGEUR = 220;
+	public static final int HAUTEUR = 500;
+	
+	
 	private JButton carre;
 	private JButton triangle;
 	private JButton cercle;
 	private JButton polygone;
+	private JButton translater;
+	private JButton modifier;
+    private JComboBox listeCouleur;
 
 	
 	public MenuVue() {
-	    
-	    //Font font = new Font("Courier", Font.BOLD,40);
+		/*
+		ImageIcon imageIcon = new ImageIcon("img/carre.png");
+		Image image = imageIcon.getImage(); // transform it 
+		Image newimg = image.getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		imageIcon = new ImageIcon(newimg);  // transform it back
+		
+		
+	    this.carre    = new JButton("Carre"   , imageIcon);
+	    this.triangle = new JButton("Triangle", new ImageIcon("img/triangle.png"));
+	    this.cercle   = new JButton("Cercle"  , new ImageIcon("img/cercle.png"));
+	    this.polygone = new JButton("Polygone", new ImageIcon("img/polygone.png"));
+	    */
+		Object[] elements = new Object[]{"Noir", "jaune", "vert", "bleu", "rouge","rose","gris","marron"};
+		
+		
 		this.setBackground(Color.pink);
-	    
-	    this.carre = new JButton("Jouer");
-	    this.triangle = new JButton("Aléatoire");
-	    this.cercle = new JButton("Reset");
-	    this.polygone = new JButton("Quitter");
+		Font font = new Font("Courier", Font.BOLD,24);
+		
+		JLabel labelCreation = new JLabel("Création : ");
+		labelCreation.setFont(font);
+		
+		JLabel labelModification = new JLabel("Modification : ");
+		labelModification.setFont(font);
+				
+		this.carre    = new JButton("Carre");
+	    this.triangle = new JButton("Triangle");
+	    this.cercle   = new JButton("Cercle");
+	    this.polygone = new JButton("Polygone");
+	    this.translater = new JButton("Translater");
+	    this.modifier = new JButton("Modifier");
+	    this.listeCouleur = new JComboBox(elements);
 	    
 	    this.carre.setPreferredSize(new Dimension(200,50));
 	    this.triangle.setPreferredSize(new Dimension(200,50));
 	    this.cercle.setPreferredSize(new Dimension(200,50));
 	    this.polygone.setPreferredSize(new Dimension(200,50));
+	    this.translater.setPreferredSize(new Dimension(200,50));
+	    this.modifier.setPreferredSize(new Dimension(200,50));
+	    this.listeCouleur.setPreferredSize(new Dimension(200,50));
+	    
 
-
+	    this.add(labelCreation);
 	    this.add(this.carre);
 	    this.add(this.triangle);
 	    this.add(this.cercle);
 	    this.add(this.polygone);
+	    this.add(labelModification);
+	    this.add(this.translater);
+	    this.add(this.modifier);
+	    this.add(this.listeCouleur);
 	    
-	    this.setPreferredSize(new Dimension(200,50));
+	    this.setPreferredSize(new Dimension(LARGEUR,HAUTEUR));
 	}
 	
 	public void setButtonLister(MenuControleur controller) {
@@ -59,6 +101,8 @@ public class MenuVue extends JPanel implements Observer {
 		this.triangle.addActionListener(controller);
 		this.cercle.addActionListener(controller);
 		this.polygone.addActionListener(controller);
+		this.modifier.addActionListener(controller);
+		this.listeCouleur.addActionListener(controller);
 
 	}
 	
@@ -79,10 +123,18 @@ public class MenuVue extends JPanel implements Observer {
 			return 4;
 		}
 		
+		if(button == this.translater) {
+			return 5;
+		}
+		
+		if(button == this.modifier) {
+			return 6;
+		}
+		
 		return -1;
 	}
 	
-	public JButton whoIsNumber(int button) {
+	private JButton whoIsNumber(int button) {
 		switch(button) {
 			case 1:
 				return this.carre;
@@ -96,6 +148,12 @@ public class MenuVue extends JPanel implements Observer {
 			case 4:
 				return this.polygone;
 				
+			case 5:
+				return this.translater;
+				
+			case 6:
+				return this.modifier;
+				
 			default:
 				return null;
 		}
@@ -108,4 +166,23 @@ public class MenuVue extends JPanel implements Observer {
 		
 	}
 	
+	public class BoutonImage extends JButton { // !!! on doit étendre le composant dans lequel on veut insérer une image de fond
+	     
+	    private Image img;
+	     
+	    //Un constructeur pour choisir plus simplement l'image
+	    public BoutonImage(String imageName) {
+	        img = new ImageIcon(getClass().getResource(imageName)).getImage();
+	    }
+	     
+	    //On doit redéfinir la méthode paintComponent() pour les composant swing !!! et paint() pour awt
+	    @Override
+	    protected void paintComponent(Graphics g) {
+	        super.paintComponent(g);
+	        if (img == null) return;
+	        g.drawImage(img, 0, 0, getWidth(), getHeight(), this); //ici on adapte l'image à la taille du composant(getWidth(), getHeight())...
+	//cf : la documentation java à http://javasearch.developpez.com/j2se/1.6.0/docs/api/java/awt/Graphics.html#drawImage(java.awt.Image,%20int,%20int,%20int,%20int,%20java.awt.image.ImageObserver)
+	     
+	    }
+	}
 }
